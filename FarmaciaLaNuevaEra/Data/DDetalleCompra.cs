848c189836dbd91ced46e427d8826537156c0a9c
Larry-Larios01 <larioslarry8@gmail.com>
2022-11-08 20:40:35 -0600

    conectando procesos de almacenado con c#

---
+++ FarmaciaLaNuevaEra/Data/DDetalleCompra.cs
@@ -0,0 +1,73 @@
+﻿using FarmaciaLaNuevaEra.Data.Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaciaLaNuevaEra.Data
+{
+    public class DDetalleCompra
+    {
+        public int  idOrdenCompra { get; set; }
+        public int idMedicamento { get; set; }
+        public int CantidadPedida { get; set; }
+
+        public DataTable Verificar_Efectivo(decimal monto)
+        {
+
+            DataTable dtResultado = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {    // Cargando el conexión al servidor
+                SqlCon.ConnectionString = Conexion.Cn;
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "verificar_efectivo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+                SqlCmd.Parameters.Add(Parametros.parametro("@monto_total", SqlDbType.Money, 0, monto));
+          
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(dtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                dtResultado = null;
+            }
+            return dtResultado;
+        }
+        public string InsertarDetalleOrdenPedido(DDetalleCompra detalleOrdencompra)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "insertar_detalleCompra";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+                SqlCmd.Parameters.Add(Parametros.parametro("@idcompra", SqlDbType.Int, 0, detalleOrdencompra.idOrdenCompra));
+                SqlCmd.Parameters.Add(Parametros.parametro("@idmedicamento", SqlDbType.Int, 0, detalleOrdencompra.idMedicamento));
+                SqlCmd.Parameters.Add(Parametros.parametro("@cantidad", SqlDbType.Int, 0, detalleOrdencompra.CantidadPedida));
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se Ingreso el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+    }
+}
---
+++ FarmaciaLaNuevaEra/Data/DOrdenCompra.cs
@@ -0,0 +1,88 @@
+﻿using FarmaciaLaNuevaEra.Data.Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaciaLaNuevaEra.Data
+{
+    public  class DOrdenCompra
+    {
+        public string Insertar_OrdenCompra(int idlabotarorio)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "insertar_ordencompra";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+                SqlCmd.Parameters.Add(Parametros.parametro("@idlab", SqlDbType.Int, 0, idlabotarorio));
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se Ingreso el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+
+        }
+        public static DataTable Ultimoidcompra()
+        {
+            DataTable dtResultado = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {    // Cargando el conexión al servidor
+                SqlCon.ConnectionString = Conexion.Cn;
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "ultimo_idcompra";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(dtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                dtResultado = null;
+            }
+            return dtResultado;
+        }
+        public static DataTable LaboratorioActivo()
+        {
+            DataTable dtResultado = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {    // Cargando el conexión al servidor
+                SqlCon.ConnectionString = Conexion.Cn;
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "mostrar_laboratorio_activo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(dtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                dtResultado = null;
+            }
+            return dtResultado;
+        }
+    }
+}
